7cd0768451e6c47f057e2dd5102f7b0ece3b3472
Rende11 <artamonovn@gmail.com>
2024-04-06 14:37:26 +0300

    Import private key main screen

--- src/status_im/contexts/wallet/add_account/create_account/import_private_key/view.cljs
+++ src/status_im/contexts/wallet/add_account/create_account/import_private_key/view.cljs
@@ -2,6 +2,7 @@
   (:require
     [clojure.string :as string]
     [quo.core :as quo]
+    [react-native.clipboard :as clipboard]
     [react-native.core :as rn]
     [status-im.common.floating-button-page.view :as floating-button-page]
     [status-im.contexts.wallet.add-account.create-account.import-private-key.style :as style]
@@ -9,67 +10,58 @@
     [utils.i18n :as i18n]
     [utils.re-frame :as rf]))
 
-(defn address-input []
-(let [ [input-value set-input-value] (rn/use-state "")]
-[quo/input
-      {:accessibility-label :add-address-to-watch
-       :placeholder         "Enter your private key";(i18n/label :t/address-placeholder)
-       :container-style     {} ;style/input
-       :label               "Private key";(i18n/label :t/eth-or-ens)
-       :type           :password
-        :error? true
-       :return-key-type     :done
-       :on-change-text      set-input-value
-       :button              (when (string/blank? input-value)
-                              {:on-press #(js/alert "ads")
-                               :text     (i18n/label :t/paste)})
-      ;;  :value               input-value
-       }]))
+(defn address-input [{:keys [input-value set-input-value error?]}]
+  (let [on-paste (rn/use-callback
+                                (fn []
+                                  (clipboard/get-string
+                                   (fn [clipboard]
+                                     (when-not (empty? clipboard)
+                                       (set-input-value clipboard))))))]
+    [quo/input
+     {:accessibility-label :add-address-to-watch
+      :placeholder         (i18n/label :t/enter-private-key-placeholder)
+      :container-style     {:margin-top 12
+                            :padding-horizontal 20}
+      :label               (i18n/label :t/private-key)
+      :type           :password
+      :error? error?
+      :return-key-type     :done
+      :on-change-text      set-input-value
+      :button              (when (string/blank? input-value)
+                             {:on-press on-paste
+                              :text     (i18n/label :t/paste)})
+      :value               input-value
+      }]))
 
 (defn view []
-   (let [customization-color (rf/sub [:profile/customization-color])]
-[rn/view {:flex 1}
-[floating-button-page/view
-     {:customization-color    customization-color
-      :header-container-style {:margin-top 8}
-      :header                 [quo/page-nav
-                               {:background :white
-                                :type       :no-title
-                                 
-                                :icon-name  :i/close
-                                :on-press   #(rf/dispatch [:navigate-back])}]
-      :footer                 [quo/button
-                               {:customization-color customization-color
-                                :disabled?           false
-                                :on-press            #()}
-                               (i18n/label :t/continue)]}
-                               
-    [rn/view {:flex 1
-      :border-width 1
-    }
-            [quo/page-top
-           {:container-style  {}
-            :title            "Import Private Key";(i18n/label :t/add-address)
-            :description      :text
-            :description-text "Enter the private key of an address";(i18n/label :t/enter-eth)
-            }]
-                [rn/view {
-                  :flex 1
-                  :justify-content :space-between
-                  :border-width 1
-                  :border-color :red
-                  :padding-horizontal 20
-    }
-       [address-input
-           {}]
-           [quo/information-box
-            {:type :default
-             :icon :i/info
-             :style {
-               :justify-self :flex-end
-               :align-self :flex-end}
-             }
-            "New addresses cannot be derived from an account imported from a private key. Import using a seed phrase if you wish to derive addresses."]
-                ]
-    ]
-                               ]]))
+  (let [customization-color (rf/sub [:profile/customization-color])
+        [input-value set-input-value] (rn/use-state "")]
+    [rn/view {:flex 1}
+     [floating-button-page/view
+      {:customization-color    customization-color
+       :header                 [quo/page-nav
+                                {:background :white
+                                 :type       :no-title
+
+                                 :icon-name  :i/close
+                                 :on-press   #(rf/dispatch [:navigate-back])}]
+       :footer                 [:<>
+                                [quo/information-box
+                                 {:type :default
+                                  :icon :i/info
+                                  :style {:margin-bottom 20}}
+                                 (i18n/label :t/import-private-key-info)]
+                                [quo/button
+                                 {:customization-color customization-color
+                                  :disabled?           (empty? input-value)
+                                  :on-press            #()}
+                                 (i18n/label :t/continue)]]}
+      [quo/page-top
+       {:container-style  {:margin-top 2}
+        :title            (i18n/label :t/import-private-key)
+        :description      :text
+        :description-text  (i18n/label :t/enter-private-key)}]
+      [address-input
+       {:input-value input-value
+        :set-input-value set-input-value
+        :error? false}]]]))
